00df9adb9ff96fa934494a6c83edee47125ca2f6
LebFre1tas <lebfre1tasfsa@gmail.com>
2025-04-15 14:59:13 +0000

    Nivel Aventureiro Concluído.

--- xadrez.c
+++ xadrez.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    
+
+
     // variáveis de controle para os loops de cada peça
     int torre = 1;
     int bispo = 1; 
-    int rainha = 1;
+    int rainha;
+    int cavalo = 1;
 
     // loop da torre
     printf(" * Movimento da torre:\n");
@@ -15,6 +17,8 @@ int main() {
         torre++;
     }
 
+    printf("\n");
+
     // loop do bispo
     printf(" * Movimento do bispo:\n");
 
@@ -23,14 +27,29 @@ int main() {
         bispo++;
     } while (bispo <= 5);
 
-
+    printf("\n");
 
     // loop da rainha
     printf(" * Movimento da rainha:\n");
 
-    for (rainha; rainha <= 8; rainha++) {
+    for (rainha = 1; rainha <= 8; rainha++) {
         printf("Esquerda.\n");
     }
 
+    printf("\n");
+
+    // loop do cavalo
+    printf(" * Movimento do cavalo:\n");
+
+    while (cavalo--) {
+        for(int i = 1; i <= 2; i++) {
+            printf("Baixo.\n");
+        }
+
+        printf("Esquerda.\n");
+    }
+
+
     return 0;
+
 }
