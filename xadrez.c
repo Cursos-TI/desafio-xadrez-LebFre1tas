4e644ab158ed9b8374c10bf01ea6d516a0b34b34
LebFre1tas <lebfre1tasfsa@gmail.com>
2025-04-19 15:41:40 +0000

    Desafio concluído.

--- xadrez.c
+++ xadrez.c
@@ -1,55 +1,75 @@
 #include <stdio.h>
 
+void movimentoTorre(int casas) {
+    if (casas > 0) {
+        printf("Direita\n");
+        movimentoTorre(casas - 1);
+    }
+}
+
+void movimentoBispo(int casas2) {
+    if (casas2 > 0) {
+        printf("Cima, direita\n");
+        movimentoBispo(casas2 - 1);
+    }
+}
+
+void movimentoRainha(int casas3) {
+    if (casas3 > 0) {
+        printf("Esquerda\n");
+        movimentoRainha(casas3 - 1);
+    }
+}
+
 int main() {
-
-
-    // variáveis de controle para os loops de cada peça
-    int torre = 1;
-    int bispo = 1; 
-    int rainha;
-    int cavalo = 1;
 
     // loop da torre
     printf(" * Movimento da torre:\n");
 
-    while (torre <= 5) {
-        printf("Direita.\n");
-        torre++;
-    }
+    movimentoTorre(5);
+    
+    printf("\n");
+
+    // loops do bispo
+    printf(" * Movimento do bispo com recursividade:\n");
+
+    movimentoBispo(5);
 
     printf("\n");
 
-    // loop do bispo
-    printf(" * Movimento do bispo:\n");
 
-    do {
-        printf("Cima, direita.\n");
-        bispo++;
-    } while (bispo <= 5);
+    printf(" * Movimento do bispo com loops aninhados:\n");
+
+    for(int bispoVertical = 1; bispoVertical <= 5; bispoVertical++) {
+        printf("Cima, ");
+        for(int bispoHorizontal = 1; bispoHorizontal <= 1; bispoHorizontal++) {
+            printf("Direita\n");
+        }
+    }
 
     printf("\n");
 
     // loop da rainha
     printf(" * Movimento da rainha:\n");
 
-    for (rainha = 1; rainha <= 8; rainha++) {
-        printf("Esquerda.\n");
-    }
+    movimentoRainha(8);
 
     printf("\n");
 
     // loop do cavalo
+    int cavalo = 1;
+
     printf(" * Movimento do cavalo:\n");
 
     while (cavalo--) {
-        for(int i = 1; i <= 2; i++) {
-            printf("Baixo.\n");
+        for(int i = 1; i >= 1 && i <= 2; i++) {
+            printf("Cima\n");
         }
 
-        printf("Esquerda.\n");
+        printf("Direita\n");
     }
 
-
+    
     return 0;
 
 }
